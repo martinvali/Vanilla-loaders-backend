d83491e0e733c89a7b3710e1d0a830a05e8ff108
Martin Väli <martinvali12@gmail.com>
2021-12-29 13:32:31 +0200

    change .find to .findById

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ app.get("/", async (req, res) => {
 
 app.get("/loaders/:id", async function (req, res) {
   const { id } = req.params;
-  const loader = await Loader.find({ id });
+  const loader = await Loader.findById({ id });
   console.log(loader);
   res.render("index", { loader });
 });
