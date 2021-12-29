b57e999824a02f4eec3e8cf31ed7b24bd278b481
Martin Väli <martinvali12@gmail.com>
2021-12-29 17:40:05 +0200

    attempt to fix a bug

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ app.get("/", async (req, res) => {
 
 app.get("/loaders/:id", async function (req, res) {
   const { id } = req.params;
-  const loader = await Loader.findById({ id: mongoose.Types.ObjectId(id) });
+  const loader = await Loader.findById({ id: mongoose.Types.ObjectID(id) });
   console.log(loader);
   res.render("index", { loader });
 });
