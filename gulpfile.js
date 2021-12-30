eee9ead806f957c52c50a6c8ae956c339ac612b8
Martin Väli <martinvali12@gmail.com>
2021-12-30 17:19:24 +0200

    add test CSS for view

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const { dest, src, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function gulpTask() {
+  return src("./scss/**/*.scss").pipe(sass()).pipe(dest("./css/styles.css"));
+}
+
+function watching() {
+  watch("scss/**/*.scss", gulpTask);
+}
+
+module.exports.default = series(gulpTask, watching);
