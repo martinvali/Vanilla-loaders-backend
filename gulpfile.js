e32b22c3c4bee320f66d1b9933ba2ad29aa0d95e
Martin Väli <martinvali12@gmail.com>
2022-01-01 14:48:05 +0200

    Add purge CSS and media queries

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const { dest, src, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function gulpTask() {
-  return src("./scss/**/*.scss").pipe(sass()).pipe(dest("./static/css"));
+  return src("./scss/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["./views/index.ejs"] }))
+    .pipe(dest("./static/css"));
 }
 
 function watching() {
