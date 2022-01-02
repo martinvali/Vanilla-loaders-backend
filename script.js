98afce0b4f5aa0d45be036b37cdf99e662a42dfd
Martin Väli <martinvali12@gmail.com>
2022-01-02 16:16:55 +0200

    add error handling to script.js

--- script.js
+++ script.js
@@ -28,7 +28,13 @@ app.get("/", async (req, res) => {
 app.get("/loaders/:id", async function (req, res) {
   const { id } = req.params;
   const loader = await Loader.findById({ _id: id });
-  fs.copyFile("./static/css/index.css", "./static/css/loaderServed.css");
+  fs.copyFile(
+    "./static/css/index.css",
+    "./static/css/loaderServed.css",
+    function (err) {
+      throw new Error(err);
+    }
+  );
   fs.appendFileSync("./static/css/loaderServed.css", loader.css, { flag: "a" });
   res.render("index", { loader });
 });
